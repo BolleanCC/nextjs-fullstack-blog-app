1f91ebfd10569f493577a07a49c638606d3913fe
BolleanCC <songtao19980820@outlook.com>
2025-06-12 11:25:06 -0600

    Optimize images

--- src/components/FeaturePosts.jsx
+++ src/components/FeaturePosts.jsx
@@ -6,7 +6,11 @@ const FeaturePosts = () => {
       {/* First */}
       <div className="w-full lg:w-1/2 flex flex-col gap-4">
         {/* image */}
-        <Image src="featured1.jpeg" className="rounded-3xl object-cover" />
+        <Image
+          src="featured1.jpeg"
+          className="rounded-3xl object-cover"
+          w="895"
+        />
         {/* details */}
         <div className="flex items-center gap-4">
           <h1 className="font-semibold lg:text-lg">01.</h1>
@@ -25,10 +29,13 @@ const FeaturePosts = () => {
       <div className="w-full lg:w-1/2 flex flex-col gap-4">
         {/* second */}
         <div className="lg:h-1/3 flex justify-between gap-4">
-          <Image
-            src="featured2.jpeg"
-            className="rounded-3xl object-cover w-1/3 aspect-video"
-          />
+          <div className="w-1/3 aspect-video">
+            <Image
+              src="featured2.jpeg"
+              className="rounded-3xl object-cover w-full h-full"
+              w="298"
+            />
+          </div>
           {/* details and title */}
           <div>
             {/* details */}
@@ -48,10 +55,13 @@ const FeaturePosts = () => {
         </div>
         {/* third */}
         <div className="lg:h-1/3 flex justify-between gap-4">
-          <Image
-            src="featured2.jpeg"
-            className="rounded-3xl object-cover w-1/3 aspect-video"
-          />
+          <div className="w-1/3 aspect-video">
+            <Image
+              src="featured3.jpeg"
+              className="rounded-3xl object-cover w-full h-full"
+              w="298"
+            />
+          </div>
           {/* details and title */}
           <div>
             {/* details */}
@@ -71,10 +81,13 @@ const FeaturePosts = () => {
         </div>
         {/* fourth */}
         <div className="lg:h-1/3 flex justify-between gap-4">
-          <Image
-            src="featured2.jpeg"
-            className="rounded-3xl object-cover w-1/3 aspect-video"
-          />
+          <div className="w-1/3 aspect-video">
+            <Image
+              src="featured4.jpeg"
+              className="rounded-3xl object-cover w-full h-full"
+              w="298"
+            />
+          </div>
           {/* details and title */}
           <div>
             {/* details */}
