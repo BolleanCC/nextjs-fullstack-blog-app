504235c9ca14482be2a5dbd2910681532aeabff6
BolleanCC <songtao19980820@outlook.com>
2025-07-03 20:29:29 -0600

    Added clerk user info to mongoDB

--- backend/index.js
+++ backend/index.js
@@ -6,8 +6,12 @@ import connectDB from "./lib/connectDB.js";
 import userRouter from "./routes/user.route.js";
 import postRouter from "./routes/post.route.js";
 import commentRouter from "./routes/comment.route.js";
+import webhookRouter from "./routes/webhook.route.js";
+import { clerkMiddleware } from "@clerk/express";
 
 const app = express();
+app.use(clerkMiddleware);
+app.use("/webhooks", webhookRouter);
 app.use(express.json());
 
 // app.get("/test", (req, res) => {
