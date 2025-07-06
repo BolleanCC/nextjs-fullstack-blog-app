0e278650bea4ad64de54c1d1af1ae50021006b62
BolleanCC <songtao19980820@outlook.com>
2025-07-06 00:41:01 -0600

    Added image upload function

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -15,6 +15,11 @@ import RegisterPage from "./routes/RegisterPage";
 import SinglePostPage from "./routes/SinglePostPage";
 import MainLayout from "./layouts/MainLayout";
 import { ClerkProvider } from "@clerk/clerk-react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import React from "react";
+import { ToastContainer } from "react-toastify";
+
+const queryClient = new QueryClient();
 
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
@@ -57,7 +62,10 @@ const router = createBrowserRouter([
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
-      <RouterProvider router={router} />
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+        <ToastContainer position="bottom-right" />
+      </QueryClientProvider>
     </ClerkProvider>
   </StrictMode>
 );
