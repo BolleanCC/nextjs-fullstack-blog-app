197662065ee6a4d2ba4013440545107938ffafa2
BolleanCC <songtao19980820@outlook.com>
2025-11-13 10:37:16 -0700

    feat: integrate comment creation with React Query mutation
    - Introduced useMutation for posting comments
    - Added handleSubmit for form data handling
    - Invalidated comment query on success to keep UI in sync
    - Included Clerk auth token in request headers

--- client/src/components/Comments.jsx
+++ client/src/components/Comments.jsx
@@ -1,35 +1,72 @@
 import Comment from "./Comment";
 
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
 import axios from "axios";
-import { comment } from "postcss";
+import { useMutation } from "@tanstack/react-query";
+import { useAuth } from "@clerk/clerk-react";
+import { toast } from "react-toastify";
 
 const fetchComments = async (postId) => {
-  const res = await axios.get(`${import.meta.env.VITE_API_URL}/posts/${slug}`);
-  return (await res).data;
+  const res = await axios.get(`${import.meta.env.VITE_API_URL}/comments/${postId}`);
+  return res.data;
 };
 
 const Comments = ({ postId }) => {
+
+  const { getToken } = useAuth();
+
   const { isPending, error, data } = useQuery({
     queryKey: ["comments", postId],
     queryFn: () => fetchComments(postId),
   });
 
+  const queryClient = useQueryClient();
+
+  const mutation = useMutation({
+    mutationFn: async (newComment) => {
+      const token = await getToken();
+      return axios.post(`${import.meta.env.VITE_API_URL}/comments/${postId}`, newComment, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+    },
+    onSuccess: (res) => {
+      toast.success("Comment has been created");
+      queryClient.invalidateQueries({ queryKey: ["comments", postId] });
+    },
+    onError: (error) => {
+      toast.error(error.response.data);
+    },
+  });
+
+
   if (isPending) return "loading...";
   if (error) return "Something went wrong.." + error.message;
   if (!data) return "Comments not found";
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    const data = {
+      desc: formData.get("desc"),
+    };
+    mutation.mutate(data);
+  };
+
   return (
-    <div className="flex flex-col gap-8 lg:w-3/5">
+    <div className="flex flex-col gap-8 lg:w-3/5 mb-12">
       <h1 className="text-xl text-gray-500 underline">Comments</h1>
-      <div className="flex items-center justify-between gap-8 w-full">
+      <form onSubmit={handleSubmit} className="flex items-center justify-between gap-8 w-full">
         <textarea
+          name="desc"
           placeholder="Write a comment..."
           className="w-full p-4 rounded-xl"
         />
         <button className="bg-blue-800 px-4 py-3 text-white font-medium rounded-xl">
           Send
         </button>
-      </div>
+      </form>
       {data.map((comment) => (
         <Comment key={comment._id} comment={comment} />
       ))}
