c50602b58c224b352594fe4a3f651a61f8b9bdbe
BolleanCC <songtao19980820@outlook.com>
2025-11-13 09:31:37 -0700

    add addComments

--- backend/controllers/comment.controller.js
+++ backend/controllers/comment.controller.js
@@ -1,9 +1,26 @@
 import Comment from "../models/comment.model.js";
+import User from "../models/user.model.js";
 
 export const getPostComments = async (req, res) => {
     const comments = await Comment.find({ post: req.params.postId }).populate("user", "username img").sort({ createdAt: -1 });
 
     res.json(comments);
 };
-export const addComments = async (req, res) => { };
+
+
+export const addComments = async (req, res) => {
+    const clerkUserId = req.auth.userId;
+    const postId = req.params.postId;
+
+    if (!clerkUserId) {
+        return res.status(401).json("Not authenticated!");
+    }
+
+    const user = await User.findOne({ clerkUserId });
+
+    const newComment = new Comment({ ...req.body, user: user._id, post: postId });
+    await newComment.save();
+
+    res.json(newComment);
+};
 export const deleteComments = async (req, res) => { };
